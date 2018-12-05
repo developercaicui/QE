331f5a7c16ce4432e684d078c1629d3c0d648fb7
XuWeijin <778553575@qq.com>
2018-12-05 10:06:27 +0800

    项目初始化。

---
+++ src/components/commonHead.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="head-wrap">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "commonHead"
+  }
+</script>
+
+<style scoped lang="stylus">
+  .head-wrap
+    width 100%
+    height 50px
+    background #7a6961
+  .hh
+    width 10px
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index-wrap">
+    <commonHead></commonHead>
+  </div>
+</template>
+
+<script>
+  import commonHead from '@/components/commonHead.vue'
+  export default {
+    name: "index",
+    components:{
+      commonHead
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
