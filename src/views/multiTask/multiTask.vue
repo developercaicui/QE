3cbb484f6e21ba281b5eb48092ff361bd7c9e104
BrightLD <1396941986@qq.com>
2018-12-05 09:34:04 +0800

    mulititask

---
+++ src/views/multiTask/multiTask.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <p>This is an about page</p>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style>
+</style>
